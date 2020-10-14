c94708cfca85a9c60fefe6b43d26de833092e24f
guillaumelenzinger <glenzinger@gmail.com>
2020-10-13 22:05:25 -0400

    date displaying with day.js

--- app.js
+++ app.js
@@ -51,18 +51,7 @@ $(window).load(function() {
 });
 
 // NEW MESSAGE PAGER
-const pagerElem = document.querySelector('#pager');
-const standbyElem = document.querySelector('#standby-screen');
-const operatorElem = document.querySelector('#operator');
-const messageElem = document.querySelector('#envelope');
 
-const scrollAmount = -700;
-window.addEventListener('scroll', (event) => {
-  const { top } = pagerElem.getBoundingClientRect();
-  const heroInView = top - window.innerHeight < scrollAmount;
-  standbyElem.style.visibility = "hidden";
-  operatorElem.style.visibility = "hidden";
-  messageElem.style.visibility = "visible";
-});
 
 // DATE
+
