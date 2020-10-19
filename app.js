fb07ec950e91b9818e01390bfcd3c2b67f114fa4
guillaumelenzinger <glenzinger@gmail.com>
2020-10-19 12:05:38 -0400

    smooth screen color changing on scroll down

--- app.js
+++ app.js
@@ -24,34 +24,36 @@ const navSlide = () => {
 navSlide();
 
 //SCROLL BACK TO TOP BUTTON
-//Get the button:
-mybutton = document.getElementById("fas fa-arrow-up");
+  //Get the button:
+  mybutton = document.getElementById("fas fa-arrow-up");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-    mybutton.style.visibility = "visible";
-  } else {
-    document.getElementById("fas fa-arrow-up").style.visibility = "hidden";
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+  function scrollFunction() {
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+      mybutton.style.visibility = "visible";
+    } else {
+      document.getElementById("fas fa-arrow-up").style.visibility = "hidden";
+    }
   }
-}
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
 
 // ICON LOADING WHILE PAGE LOADS
-// Wait for window load
-$(window).load(function() {
-  // Animate loader off screen
-  $(".se-pre-con").fadeOut("slow");;
-});
+  // Wait for window load
+  $(window).load(function() {
+    // Animate loader off screen
+    $(".se-pre-con").fadeOut("slow");;
+  });
 
-// NEW MESSAGE PAGER
+// NEW MESSAGE PAGER ON SCROLL DOWN
 
-
+  
 // DATE
+
+
 
