2dcd1adb83445db72c266384ba9fe599e10eaaa0
guillaumelenzinger <glenzinger@gmail.com>
2020-10-21 16:04:18 -0400

    coding pager + menu

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ navSlide();
   // When the user scrolls down 20px from the top of the document, show the button
   window.onscroll = function() {scrollFunction()};
   function scrollFunction() {
-    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
       mybutton.style.visibility = "visible";
     } else {
       document.getElementById("fas fa-arrow-up").style.visibility = "hidden";
